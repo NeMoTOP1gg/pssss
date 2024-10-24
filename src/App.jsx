cc81e0b37d06e02df889036643431a56f3e88027
Cybersport <Cybersport@26kadr.internal>
2024-10-24 14:52:35 +0300

    pssss

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,38 @@ import './App.css';
 import logo from '../src/images/Logo.svg'
 import search from '../src/images/Search.svg'
 import cart from '../src/images/Cart.svg'
+import photo from '../src/images/photo.svg'
 
 
 function App() {
   return (
     <>
-      <div className="header">
-        <img src={logo} alt="logo"></img>
-        <p>Organik</p>
-        <ul className='tab'>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Pages</a></li>
-          <li><a href="#">Shop</a></li>
-          <li><a href="#">Project</a></li>
-          <li><a href="#">News</a></li>
-        </ul>
-        <div className="block_input">
-          <input type="text" />
-          <img src={search} alt='#' />
+      <div className="main">
+        <div className="header">
+          <img src={logo} alt="logo"></img>
+          <p>Organik</p>
+          <ul className='tab'>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Pages</a></li>
+            <li><a href="#">Shop</a></li>
+            <li><a href="#">Project</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+          <div className="block_input">
+            <input type="text" />
+            <img src={search} alt='#' />
+          </div>
+          <div className="cart" >
+            <img src={cart} alt='#' />
+            <p>cart(0)</p>
+          </div>
+        </div >
+        <div className='container'>
+          <img src={photo} alt="img"></img>
         </div>
-        <div className="cart" >
-        <img src={cart} alt='#' />
-        <p>cart(0)</p>
       </div>
-    </div >
-  </>
+    </>
   );
 }
 
